95f52fdff48d855a21b0699d96375de9eb2d3d30
joomkit <info@joomkit.com>
2020-11-01 18:09:08 +0000

    offcanvas homepage

--- src/js/jscustom.js
+++ src/js/jscustom.js
@@ -1,9 +1,7 @@
 //@codekit-prepend "prepend/vendor/jquery/jquery-3.3.1.js"
-//@codekit-prepend "prepend/vendor/bootstrap/bootstrap.js"
-
-
 //@codekit-prepend "prepend/vendor/popper.min.js"
-
+// @codekit-prepend "prepend/vendor/bootstrap/bootstrap.js"
 //@codekit-prepend "prepend/vendor/flickity.js"
 //@codekit-prepend "prepend/vendor/match-height.js"
+
 //@codekit-prepend "prepend/custom/customjs.js"
