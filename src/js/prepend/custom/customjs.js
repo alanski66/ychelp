0fdb57bd0b64b9444cac1e38eef8a8ade756fb13
joomkit <info@joomkit.com>
2020-11-10 12:44:08 +0000

    fix page useful links
    added keybaord aware smooth scroll focus and count display

--- src/js/prepend/custom/customjs.js
+++ src/js/prepend/custom/customjs.js
@@ -42,17 +42,40 @@ $(document).ready(function(){
         // $('.offcanvas-collapse').toggleClass('open');
     })
 
-// animation scroll js
-var html_body = $('html, body');
-$('.scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
-    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-            html_body.animate({
-                scrollTop: target.offset().top - 90
-            }, 1500, 'easeInOutExpo');
-            return false;
+
+// URL updates and the element focus is maintained for keyboard navigation
+// originally found via in Update 3 on http://www.learningjquery.com/2007/10/improved-animated-scrolling-script-for-same-page-links
+
+// filter handling for a /dir/ OR /indexordefault.page
+function filterPath(string) {
+    return string
+        .replace(/^\//, '')
+        .replace(/(index|default).[a-zA-Z]{3,4}$/, '')
+        .replace(/\/$/, '');
+}
+
+var locationPath = filterPath(location.pathname);
+$('a.scroll').each(function () {
+    var thisPath = filterPath(this.pathname) || locationPath;
+    var hash = this.hash;
+    if ($("#" + hash.replace(/#/, '')).length) {
+        if (locationPath == thisPath && (location.hostname == this.hostname || !this.hostname) && this.hash.replace(/#/, '')) {
+            var $target = $(hash), target = this.hash;
+            if (target) {
+                $(this).click(function (event) {
+                    event.preventDefault();
+                    $('html, body').animate({scrollTop: $target.offset().top}, 1000, function () {
+                        location.hash = target;
+                        $target.focus();
+                        if ($target.is(":focus")){ //checking if the target was focused
+                            return false;
+                        }else{
+                            $target.attr('tabindex','-1'); //Adding tabindex for elements not focusable
+                            $target.focus(); //Setting focus
+                        };
+                    });
+                });
+            }
         }
     }
 });
